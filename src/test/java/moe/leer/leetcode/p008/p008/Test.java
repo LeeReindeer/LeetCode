0edcbdb4e8b77499e3381d315e6e74ad87a8de03
LeeReindeer <reindeerlee.work@gmail.com>
2019-01-27 18:24:05 +0800

    :tada: init commit, passed 8

---
+++ src/test/java/moe/leer/leetcode/p008/p008/Test.java
@@ -0,0 +1,29 @@
+package moe.leer.leetcode.p008.p008;
+
+import moe.leer.leetcode.p008.Solution;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author leer
+ * Created at 1/27/19 6:12 PM
+ */
+public class Test {
+
+  @org.junit.Test
+  public void testAtoi() {
+    Solution solution = new Solution();
+
+    assertEquals(solution.myAtoi("0000000000012345678"), 12345678);
+    assertEquals(solution.myAtoi("+0000000000012345678"), 12345678);
+    assertEquals(solution.myAtoi("-"), 0);
+    assertEquals(solution.myAtoi("-+1"), 0);
+    assertEquals(solution.myAtoi("42"), 42);
+    assertEquals(solution.myAtoi("-42"), -42);
+    assertEquals(solution.myAtoi("     -42"), -42);
+    assertEquals(solution.myAtoi("4193 with words"), 4193);
+    assertEquals(solution.myAtoi("words and 987"), 0);
+    assertEquals(solution.myAtoi(String.valueOf(Integer.MAX_VALUE)), Integer.MAX_VALUE);
+    assertEquals(solution.myAtoi(String.valueOf((long) Integer.MAX_VALUE + 1)), Integer.MAX_VALUE);
+  }
+}
