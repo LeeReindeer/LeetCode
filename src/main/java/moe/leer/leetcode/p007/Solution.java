da5547dd55268e6ddea8d8332c93b184bb54384c
LeeReindeer <reindeerlee.work@gmail.com>
2019-04-29 18:57:13 +0800

    :wrench: Complete problem description

--- src/main/java/moe/leer/leetcode/p007/Solution.java
+++ src/main/java/moe/leer/leetcode/p007/Solution.java
@@ -5,6 +5,7 @@ package moe.leer.leetcode.p007;
  * Created at 4/29/19 6:38 PM
  *
  * Given a 32-bit signed integer, reverse digits of an integer.
+ *  assume that your function returns 0 when the reversed integer overflows.
  */
 public class Solution {
   public int reverse(int x) {
