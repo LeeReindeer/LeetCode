c9a7833dd8a06d1abd7d39e08be34f6e82c7431b
LeeReindeer <LeeReix@live.com>
2019-06-11 19:28:45 +0800

    [Medium] 102. Binary Tree Level Order Traversal More concise code

--- src/main/java/moe/leer/leetcode/P102/Solution.java
+++ src/main/java/moe/leer/leetcode/P102/Solution.java
@@ -17,29 +17,20 @@ class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     if (root == null) return ans;
     q.add(root);
-    List<Integer> level1 = new LinkedList<>();
-    level1.add(root.val);
-    ans.add(level1);
-    // nodes count in current level
-    int children = 1;
     while (!q.isEmpty()) {
       List<Integer> level = new LinkedList<>();
       // nodes count in next level
-      int next = 0;
+      int children = q.size();
       while (children-- > 0) {
         TreeNode node = q.poll();
         if (node.left != null) {
           q.add(node.left);
-          next++;
-          level.add(node.left.val);
         }
         if (node.right != null) {
           q.add(node.right);
-          next++;
-          level.add(node.right.val);
         }
+        level.add(node.val);
       }
-      children = next;
       if (level.size() != 0) ans.add(level);
     }
     return ans;
