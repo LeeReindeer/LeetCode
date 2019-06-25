ca3973209d9e54f6e80ddaea4d42baff747de9ea
LeeReindeer <LeeReix@live.com>
2019-06-25 15:02:46 +0800

    [Medium] 127. Word Ladder Fix calculate list size

--- src/main/java/moe/leer/leetcode/p046/Solution.java
+++ src/main/java/moe/leer/leetcode/p046/Solution.java
@@ -8,12 +8,20 @@ import java.util.List;
  * @author leer
  * Created at 6/25/19 1:51 PM
  * <p>
+ * 46. Permutations
  * Given a collection of distinct integers, return all possible permutations.
+ * <p>
+ * The number of permutations is n!/(n-r)! = n!(cause n == r)
  */
 public class Solution {
 
+
   public List<List<Integer>> permute(int[] nums) {
-    List<List<Integer>> permutes = new ArrayList<>(nums.length * nums.length - 1);
+    int size = 1;
+    for (int i = 2; i <= nums.length; i++) {
+      size *= i;
+    }
+    List<List<Integer>> permutes = new ArrayList<>(size);
     if (nums.length == 0) return permutes;
     LinkedList<Integer> aPermute = new LinkedList<>();
     boolean[] visited = new boolean[nums.length];
@@ -41,6 +49,7 @@ public class Solution {
   public static void main(String[] args) {
     Solution solution = new Solution();
     List<List<Integer>> permutes = solution.permute(new int[]{1, 2, 3});
+    System.out.println(permutes.size());
     for (List<Integer> list : permutes) {
       System.out.println(list.toString());
     }
