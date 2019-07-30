9c6af83c3ff05cc96669f540a78d4cef672d1a7c
LeeReindeer <LeeReix@live.com>
2019-07-30 23:09:38 +0800

    [Medium] 15. 3Sum

--- src/main/java/moe/leer/leetcode/p015/Solution.java
+++ src/main/java/moe/leer/leetcode/p015/Solution.java
@@ -20,7 +20,7 @@ import java.util.List;
  * The solution set must not contain duplicate triplets.
  */
 public class Solution {
-  public List<List<Integer>> threeSum(int[] nums) {
+  public List<List<Integer>> threeSum2(int[] nums) {
     List<List<Integer>> ans = new ArrayList<>();
     Arrays.sort(nums);
     backtracking(ans, new LinkedList<>(), nums, new boolean[nums.length], 0);
@@ -46,6 +46,36 @@ public class Solution {
       current.remove(new Integer(nums[i]));
       visited[i] = false;
     }
+  }
+
+
+  /**
+   * 1. sort the array
+   * 2. select first element
+   * 3. do bi-directional 2sum sweep
+   */
+  public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 2; i++) {
+      if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+        // find 2sum equals to 0 - first
+        int lo = i + 1, hi = nums.length - 1, twoSum = 0 - nums[i];
+        while (lo < hi) {
+          if (nums[lo] + nums[hi] == twoSum) {
+            ans.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+            while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+            while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+            lo++;hi--;
+          } else if (nums[lo] + nums[hi] < twoSum) {
+            lo++;
+          } else {
+            hi--;
+          }
+        }
+      }
+    }
+    return ans;
   }
 
   public static void main(String[] args) {
