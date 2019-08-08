3475b7baf4291962a0f1a8ad496f3613d15e9766
LeeReindeer <LeeReix@live.com>
2019-08-08 16:51:08 +0800

    [Medium] 322. Coin Change

--- src/main/java/moe/leer/leetcode/p322/Solution.java
+++ src/main/java/moe/leer/leetcode/p322/Solution.java
@@ -7,6 +7,11 @@ import java.util.Queue;
 /**
  * @author leer
  * Created at 6/12/19 10:01 AM
+ * <p>
+ *  322. Coin Change
+ * You are given coins of different denominations and a total amount of money amount.
+ * Write a function to compute the fewest number of coins that you need to make up that amount.
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
  */
 public class Solution {
   // @see https://leetcode.com/articles/coin-change/
