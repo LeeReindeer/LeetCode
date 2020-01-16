ef7d411973ff395521d7af9e1e3287ec2feed397
LeeReindeer <LeeReix@live.com>
2020-01-16 16:39:21 +0800

    [Medium] 88. Merge Sorted Array

--- src/main/java/moe/leer/leetcode/p088/Solution.java
+++ src/main/java/moe/leer/leetcode/p088/Solution.java
@@ -35,11 +35,27 @@ public class Solution {
     }
   }
 
+  // no aux array, from tail to head, non-overlapping
+  public void merge2(int[] nums1, int m, int[] nums2, int n) {
+    int index1 = m - 1, index2 = n - 1, p = m + n - 1;
+    while (index1 >= 0 || index2>= n) {
+      if (index1 < 0) {
+        nums1[p--] = nums2[index2--];
+      } else if (index2 < 0) {
+        nums1[p--] = nums1[index1--];
+      } else if (nums1[index1] > nums2[index2]) {
+        nums1[p--] = nums1[index1--];
+      } else {
+        nums1[p--] = nums2[index2--];
+      }
+    }
+  }
+
   public static void main(String[] args) {
     Solution solution = new Solution();
     int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
     int[] nums2 = new int[]{2, 5, 6};
-    solution.merge(nums1, 3, nums2, 3);
+    solution.merge2(nums1, 3, nums2, 3);
     Util.printArray(nums1);
   }
 }
